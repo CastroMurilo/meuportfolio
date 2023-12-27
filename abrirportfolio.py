c1515c2c5b0a60a5fa2f9b546e337bc008d1b311
Murilo da Rosa Castro <112021574+CastroMurilo@users.noreply.github.com>
2023-12-27 12:27:50 -0300

    Update abrirportfolio.py

--- abrirportfolio.py
+++ abrirportfolio.py
@@ -1,15 +1,32 @@
 import pyautogui as pa
 import time
+
+# Define o FAILSAFE como True, o que significa que mover o mouse para o canto superior esquerdo irá gerar uma exceção e interromper o programa.
+pa.FAILSAFE = True
+
+# Define a pausa entre as funções do PyAutoGUI como 1 segundo.
 pa.PAUSE = 1
 
+# Pressiona as teclas Win+R para abrir a caixa de diálogo Executar.
+pa.hotkey("win", "r")
 
-pa.press('win')
-pa.write('chrome')
-pa.press('ENTER')
-pa.write('www.linkedin.com/in/murilorosacastro')
-pa.press('ENTER')
-time.sleep(4)
-pa.rightClick(x=2649, y=552)
-time.sleep(2)
-pa.click(x=2708, y=334)
+# Digita a URL do perfil do LinkedIn na caixa de diálogo Executar e pressiona Enter.
+pa.typewrite(" https://www.linkedin.com/in/murilorosacastro \n")
 
+# Aguarda 5 segundos para permitir o carregamento da página do LinkedIn.
+time.sleep(5)
+
+# Clica com o botão direito na imagem "portfolio.png" na tela (com um nível de confiança de 0.8) e exibe o menu de contexto para a localização clicada com o botão direito.
+pa.rightClick(pa.locateCenterOnScreen("img\\portfolio.png", confidence=0.8), duration=1)
+
+# Clica na imagem "abrir.png" na tela (com um nível de confiança de 0.8) para abrir a opção do menu de contexto.
+pa.click(pa.locateCenterOnScreen("img\\abrir.png", confidence=0.8), duration=1)
+
+# Aguarda 1 segundo.
+time.sleep(1)
+
+# Clica na imagem "seguir.png" na tela (com um nível de confiança de 0.8).
+pa.click(pa.locateCenterOnScreen("img\\seguir.png", confidence=0.8), duration=1)
+
+# Exibe um alerta com a mensagem, título e texto do botão especificados.
+pa.alert(text="Este é meu portfólio", title="Aviso do sistema", button="OK")
